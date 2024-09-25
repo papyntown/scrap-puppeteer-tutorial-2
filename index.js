a62f8dbbbe6a8543fbfc35a64ecb35f80f3d1904
Antony Baills <a.baills@lookupspace.com>
2024-09-25 14:49:54 +0200

    wip

--- index.js
+++ index.js
@@ -15,11 +15,18 @@ await page.goto(
     "https://www.amazon.fr/s?k=amazon+basics&__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=A349GJUSPT90&sprefix=amazon+basics%2Caps%2C125&ref=nb_sb_noss_2"
 );
 
-const productsHandles = await page.$$(".a-section .a-spacing-base");
+const productsHandles = await page.$$(
+    ".s-main-slot .s-result-list .s-search-results .sg-row"
+);
 
 for (const productHandle of productsHandles) {
-    const product = await page.evaluate((el) => el.innerText, productHandle);
-    console.log(product);
-}
+    // Chercher le lien <a> dans l'élément
+    const product = await page.evaluate((el) => {
+        console.log(el, "element");
 
+        return el.innerHTML;
+    }, productHandle);
+    console.log(product, "ow");
+}
+console.log(productsHandles);
 // await browser.close();
